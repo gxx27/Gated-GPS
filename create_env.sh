499fee6c646506311555971566eb611102b1d1a7
NobHappy <1402550652@qq.com>
2025-10-19 19:49:59 +0000

    fix mkl error

--- create_env.sh
+++ create_env.sh
@@ -4,6 +4,8 @@ source activate GatedGPS
 
 conda install -y cudatoolkit=11.8
 
+conda install mkl=2023.1.0 -y # install if missing in torch environment
+
 conda install -y pytorch==2.2.0 torchvision==0.17.0 torchaudio==2.2.0 pytorch-cuda=11.8 -c pytorch -c nvidia
 
 pip uninstall numpy -y
